646c45bb075eef5bd03fae437521a63151b6ab0a
DannyIGuesss <danielgarcia448353@gmail.com>
2023-02-03 12:52:13 -0800

    Using axios instead of fetch

--- Week4/axios-pokemon-api/src/App.js
+++ Week4/axios-pokemon-api/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-
+import axios from "axios"
 import {useState, useEffect} from 'react'
 
 function App() {
@@ -8,16 +8,25 @@ function App() {
 
 //We are making the api call here and using useEffect (lets react know we need to do something after render)
 //we also use an empty array so that it only loads when the page loads
+  // useEffect(() => {
+  //   fetch('https://pokeapi.co/api/v2/pokemon?limit=807&offset=0')
+  //   //here we are using the .json to convert it into js so we can use it
+  //   .then(result => result.json())
+  //   .then((result) =>  setpokemonInfo(result.results))
+  //   .catch((err) => {console.log(err);})
+  // }, [])
+
+  //using Axios
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=807&offset=0')
-    //here we are using the .json to convert it into js so we can use it
-    .then(result => result.json())
-    .then((result) =>  setpokemonInfo(result.results))
-    .catch((err) => {console.log(err);})
-  }, [])
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=807&offset=0')
+    .then(response => setpokemonInfo(response.data.results))
+    .catch(err => console.log(err))
+  },[])
+  
 
   return (
     <div>
+      <h1 style={{textAlign: 'center'}}>All the Pokemons</h1>
       {
         pokemonInfo.map((pokemon) => (
           <p className='center'>{pokemon.name}</p>
