d039ff0143f350fe995c68fc3d75a9bd22e55ba9
DannyIGuesss <danielgarcia448353@gmail.com>
2023-02-11 14:47:52 -0800

    add the "app" in line 9

--- Week5/Jokes/server/server.js
+++ Week5/Jokes/server/server.js
@@ -6,6 +6,6 @@ require("./config/mongoose.config")
 app.use(express.json(), express.urlencoded({ extended: true }));
 
 const AllJokeRoutes = require('./routes/jokes.routes')
-AllJokeRoutes()
+AllJokeRoutes(app)
 
 app.listen(8000, () => console.log("The server is all fired up on port 8000"));
