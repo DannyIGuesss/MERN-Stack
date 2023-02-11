b632f8ede986cf5f3618379cafab9b5ebdb63beb
DannyIGuesss <danielgarcia448353@gmail.com>
2023-02-11 14:41:28 -0800

    making all the routes for the server

---
+++ Week5/Jokes/server/routes/jokes.routes.js
@@ -0,0 +1,9 @@
+const jokeController = require('../controllers/jokes.controller')
+
+module.exports = app => {
+    app.get('/api/jokes', jokeController.allJokes)
+    app.get('/api/jokes/:id', jokeController.oneJoke)
+    app.put('/api/jokes/:id', jokeController.updateJoke)
+    app.post('/api/jokes', jokeController.createNewJoke)
+    app.delete('/api/joke/:id', jokeController.deleteJoke)
+}
