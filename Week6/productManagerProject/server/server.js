fc32cdd87d59bf1d0b77f7970ef4c43c50102e35
DannyIGuesss <danielgarcia448353@gmail.com>
2023-02-23 15:19:56 -0800

    .

--- Week6/productManagerProject/server/server.js
+++ Week6/productManagerProject/server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
-const port = 8000;
-app.use(cors())
 
+app.use(cors())
+const port = 8000;
 require('./config/mongoose.config');
 
 app.use(express.json(), express.urlencoded({ extended: true }));
